fe316843b2e66625e5cb052706829868faf54358
Stanley Wu <stanley@ankr.network>
2019-01-01 18:09:46 -0800

    [SWDEV-117] re-format

--- constants.go
+++ constants.go
@@ -1,10 +1,14 @@
 package ankr_const
 
+// RabbitQ name
 const TaskManagerQueueName = "task_manager"
+
+// Task Events
 const NewTaskEvent = "New"
 const UpdateTaskEvent = "Update"
 const CancelTaskEvent = "Cancel"
 
+// Hub task status
 const TaskStatusNew = "new"
 const TaskStatusRunning = "running"
 const TaskStatusStartFailed = "startFailed"
@@ -13,7 +17,7 @@ const TaskStatusUpdateFailed = "updateFailed"
 const TaskStatusCancelled = "cancelled"
 const TaskStatusDone = "done"
 
-
+// Data center task status
 const DataCenterTaskStartSuccess = "StartSuccess"
 const DataCenterTaskStartFailure = "StartFailure"
 const DataCenterTaskUpdateSuccess = "UpdateSuccess"
@@ -21,27 +25,28 @@ const DataCenterTaskUpdateFailure = "UpdateFailure"
 const DataCenterTaskDone = "Done"
 const DataCenterTaskCancelled = "Cancelled"
 
-const CliReplyStatusSuccess = "Success"
-const CliReplyStatusFailure = "Failure"
-
-const DataCenterName = "datacenter_1"
-
+// Data center status
 const DataCenteStatusOnLine = "available"
 const DataCenteStatusOffLine = "unavailable"
 
+// CLI reply status
+const CliReplyStatusSuccess = "Success"
+const CliReplyStatusFailure = "Failure"
 
-const TaskHidden = "hidden"
-
-
+// CLI errors
 const CliErrorReasonDataCenterNotExist = "DataCenter does not exist"
 const CliErrorReasonUserNotExist = "User does not exist"
 const CliErrorReasonTaskNotExist = "Task does not exist"
 const CliErrorReasonUserNotOwn = "User does not own this task"
 const CliErrorReasonUpdateFailed = "Task can not be updated"
 
+// To do: Remove this line to generate data center name automatically
+const DataCenterName = "datacenter_1"
 
-const DefaultPort = "50051"
-
+// To do: Remove this line when usrmgr is ready
 const DefaultUserToken = "ed1605e17374bde6c68864d072c9f5c9"
 
-const HeartBeatTimeOut = 30
+const TaskHidden = "hidden"
+const DefaultPort = 50051      // Default port for gRPC connection
+const ClientTimeOut = 30       // Default timeout for client connection
+const MicroServiceTimeOut = 10 // Default timeout for internal micro service connection
