19116e3feb085bed19e60a986b4ec2b994df62e6
yousongzhang <yzhang303@dons.usfca.edu>
2019-01-17 19:14:45 -0800

    datacenter-metrics

--- constants.go
+++ constants.go
@@ -57,3 +57,17 @@ const DefaultPort = 50051      // Default port for gRPC connection
 const HeartBeatInterval = 30   // Default interval for heartbeating
 const ClientTimeOut = 60       // Default timeout for client connection
 const MicroServiceTimeOut = 10 // Default timeout for internal micro service connection
+
+
+//DataCenter Metrics
+type Metrics struct {
+	TotalCPU     int64
+	UsedCPU      int64
+	TotalMemory  int64
+	UsedMemory   int64
+	TotalStorage int64
+	UsedStorage  int64
+	ImageCount    int64
+	EndPointCount int64
+	NetworkIO     int64
+}
