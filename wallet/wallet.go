be50693767914f72fe0b201dbd4820fd26801dba
song-liu <van20052005@hotmail.com>
2019-02-16 06:58:38 +0000

    SWDEV-133 add GetAddressByPublicKey API and test case

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -32,8 +32,16 @@ const PubkeyStart = "PubKeyEd25519{"
 */
 func GenerateKeys() (priv_key_base64, pub_key_base64, address string) {
 	mykey := ed25519.GenPrivKey()
-	//mykey := [194 108 153 102 131 30 117 105 108 61 64 213 8 236 190 78 37 92 172 128 79 114 125 214 36 223 36 229 195 208 128 139 194 241 198 220 71 93 5 181 208 29 204 137 106 93 2 75 246 16 112 214 45 17 177 88 197 232 231 169 255 78 132 206]
-	//mykey := ed25519.PrivKeyEd25519{194,108, 153, 102, 131, 30, 117, 105, 108, 61, 64, 213, 8, 236, 190, 78, 37, 92, 172, 128, 79, 114, 125, 214, 36, 223, 36, 229, 195, 208, 128, 139, 194, 241, 198, 220, 71, 93, 5, 181, 208, 29, 204, 137, 106, 93, 2, 75, 246, 16, 112, 214, 45, 17, 177, 88, 197, 232, 231, 169, 255, 78, 132, 206}
+
+	// data for test
+	//mykey := [194 108 153 102 131 30 117 105 108 61 64 213 8 236 190 78 37 92 172 128 
+	//79 114 125 214 36 223 36 229 195 208 128 139 194 241 198 220 71 93 5 181 208 29 204 
+	//137 106 93 2 75 246 16 112 214 45 17 177 88 197 232 231 169 255 78 132 206]
+	//mykey := ed25519.PrivKeyEd25519{194,108, 153, 102, 131, 30, 117, 105, 108, 61, 64, 213, 
+	//8, 236, 190, 78, 37, 92, 172, 128, 79, 114, 125, 214, 36, 223, 36, 229, 195, 208, 128, 
+	//139, 194, 241, 198, 220, 71, 93, 5, 181, 208, 29, 204, 137, 106, 93, 2, 75, 246, 16, 
+	//112, 214, 45, 17, 177, 88, 197, 232, 231, 169, 255, 78, 132, 206}
+
 	privArray := [PrivKeyEd25519Size]byte(mykey)
 	privBytes := privArray[:]
 	privB64 := base64.StdEncoding.EncodeToString(privBytes)
@@ -51,6 +59,20 @@ func GenerateKeys() (priv_key_base64, pub_key_base64, address string) {
 
 	address = fmt.Sprintf("%s", mykey.PubKey().Address())
 	return priv_key_base64, pub_key_base64, address
+}
+
+/*
+  Get address by public key.
+  With this function, if a user has public_key, address is not very necessary to save in database.
+*/
+func GetAddressByPublicKey(pub_key string) (string, error) {
+	pubKeyObject, err := deserilizePubKey(pub_key)
+	if err != nil {
+		return "", err
+	}
+
+	address := fmt.Sprintf("%s", pubKeyObject.Address())
+	return address, nil
 }
 
 /*
@@ -172,7 +194,26 @@ func deserilizePrivKey(priv_key_b64 string) (ed25519.PrivKeyEd25519, error) {
 	}
 
 	pp := []byte(kDec)
-	keyObject := ed25519.PrivKeyEd25519{pp[0], pp[1], pp[2], pp[3], pp[4], pp[5], pp[6], pp[7], pp[8], pp[9], pp[10], pp[11], pp[12], pp[13], pp[14], pp[15], pp[16], pp[17], pp[18], pp[19], pp[20], pp[21], pp[22], pp[23], pp[24], pp[25], pp[26], pp[27], pp[28], pp[29], pp[30], pp[31], pp[32], pp[33], pp[34], pp[35], pp[36], pp[37], pp[38], pp[39], pp[40], pp[41], pp[42], pp[43], pp[44], pp[45], pp[46], pp[47], pp[48], pp[49], pp[50], pp[51], pp[52], pp[53], pp[54], pp[55], pp[56], pp[57], pp[58], pp[59], pp[60], pp[61], pp[62], pp[PrivKeyEd25519Size-1]}
+	keyObject := ed25519.PrivKeyEd25519{pp[0], pp[1], pp[2], pp[3], pp[4], pp[5], pp[6], pp[7], pp[8], pp[9],
+		pp[10], pp[11], pp[12], pp[13], pp[14], pp[15], pp[16], pp[17], pp[18], pp[19], pp[20], pp[21],
+		pp[22], pp[23], pp[24], pp[25], pp[26], pp[27], pp[28], pp[29], pp[30], pp[31], pp[32], pp[33],
+		pp[34], pp[35], pp[36], pp[37], pp[38], pp[39], pp[40], pp[41], pp[42], pp[43], pp[44], pp[45],
+		pp[46], pp[47], pp[48], pp[49], pp[50], pp[51], pp[52], pp[53], pp[54], pp[55], pp[56], pp[57],
+		pp[58], pp[59], pp[60], pp[61], pp[62], pp[PrivKeyEd25519Size-1]}
 
 	return keyObject, nil
+}
+
+func deserilizePubKey(pub_key_b64 string) (ed25519.PubKeyEd25519, error) {
+	pDec, err := base64.StdEncoding.DecodeString(pub_key_b64)
+	if err != nil {
+		return ed25519.PubKeyEd25519{}, err
+	}
+
+	pk := []byte(pDec)
+	var pubObject ed25519.PubKeyEd25519 = ed25519.PubKeyEd25519{pk[0], pk[1], pk[2], pk[3],pk[4], pk[5],pk[6],
+		pk[7],pk[8], pk[9], pk[10], pk[11], pk[12], pk[13], pk[14], pk[15], pk[16], pk[17], pk[18], pk[19],
+		pk[20], pk[21],pk[22], pk[23],pk[24], pk[25],pk[26], pk[27],pk[28], pk[29],pk[30], pk[PubKeyEd25519Size - 1]}
+
+	return pubObject, nil
 }
