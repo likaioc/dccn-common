2c1478bf90821415dc84c8643d96ab53d3494608
song-liu <van20052005@hotmail.com>
2019-04-14 00:38:51 +0000

    SWDEV-212 add two more APIs, test data, test commands readme.

--- cert/cert.go
+++ cert/cert.go
@@ -7,12 +7,14 @@ import (
 	"crypto/x509/pkix"
 	"encoding/pem"
 	"log"
-	_ "math/big"
-	_ "os"
+	_"math/big"
+	_"os"
 	"time"
-	_ "fmt"
+	_"fmt"
 	"bytes"
 	"github.com/hashicorp/vault/helper/certutil"
+
+	"crypto/tls"
 )
 
 // create a CA
@@ -28,11 +30,11 @@ func GenerateCA(name string) (cert string, priv_key string, err error) {
 		SerialNumber: sn,
 		Subject: pkix.Name{
 			CommonName: name,
-			Organization:  []string{"DEV"},
+			Organization:  []string{"HUBCA"},
 			Country:       []string{"US"},
 			Province:      []string{"CA"},
 			Locality:      []string{"SF"},
-			StreetAddress: []string{"mission st."},
+			StreetAddress: []string{"MISSION ST."},
 			PostalCode:    []string{"94105"},
 		},
 		NotBefore:             time.Now(),
@@ -41,6 +43,7 @@ func GenerateCA(name string) (cert string, priv_key string, err error) {
 		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
 		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
 		BasicConstraintsValid: true,
+		//AuthorityKeyId:      []byte{1, 2, 3, 4, 6},
 	}
 
 	priv, _ := rsa.GenerateKey(rand.Reader, 2048)
@@ -55,6 +58,120 @@ func GenerateCA(name string) (cert string, priv_key string, err error) {
 	pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: ca_b})
 	//fmt.Println(certOut.String())
 
+	keyOut := new(bytes.Buffer)
+	pem.Encode(keyOut, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)})
+	//fmt.Println(keyOut.String())
+
+	return certOut.String(), keyOut.String(), nil
+}
+
+// Generate a cert
+// input: name, certCAPem, keyCAPem
+// output: server cert, priv_key
+func GenerateServerCert(name, certCAPem, keyCAPem string) (scert string, priv_key string, err error) {
+	// Load CA
+	catls, err := tls.X509KeyPair([]byte(certCAPem), []byte(keyCAPem))
+	if err != nil {
+		return "", "", err
+	}
+	ca, err := x509.ParseCertificate(catls.Certificate[0])
+	if err != nil {
+		return "", "", err
+	}
+
+	sn, err := certutil.GenerateSerialNumber()
+	if err != nil {
+		return "", "", err
+	}
+
+	cert := &x509.Certificate{
+		SerialNumber: sn,
+		Subject: pkix.Name{
+			CommonName: name,
+			Organization:  []string{"HUB"},
+			Country:       []string{"US"},
+			Province:      []string{"CA"},
+			Locality:      []string{"SF"},
+			StreetAddress: []string{"MISSION ST."},
+			PostalCode:    []string{"94105"},
+		},
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().AddDate(10, 0, 0),
+		//SubjectKeyId: []byte{1, 2, 3, 4, 6},
+		//ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		KeyUsage:     x509.KeyUsageDigitalSignature,
+		IsCA:                  false,
+		BasicConstraintsValid: true,
+	}
+	priv, _ := rsa.GenerateKey(rand.Reader, 2048)
+	pub := &priv.PublicKey
+
+	// Sign the certificate
+	cert_b, err := x509.CreateCertificate(rand.Reader, cert, ca, pub, catls.PrivateKey)
+
+	// Public key
+	certOut := new(bytes.Buffer)
+	pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: cert_b})
+	//fmt.Println(certOut.String())
+
+	// Private key
+	keyOut := new(bytes.Buffer)
+	pem.Encode(keyOut, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)})
+	//fmt.Println(keyOut.String())
+
+	return certOut.String(), keyOut.String(), nil
+}
+
+// Generate a client cert
+// input: name, certCAPem, keyCAPem
+// output: server cert, priv_key
+func GenerateClientCert(name, certCAPem, keyCAPem string) (scert string, priv_key string, err error) {
+	// Load CA
+	catls, err := tls.X509KeyPair([]byte(certCAPem), []byte(keyCAPem))
+	if err != nil {
+		return "", "", err
+	}
+	ca, err := x509.ParseCertificate(catls.Certificate[0])
+	if err != nil {
+		return "", "", err
+	}
+
+	sn, err := certutil.GenerateSerialNumber()
+	if err != nil {
+		return "", "", err
+	}
+
+	cert := &x509.Certificate{
+		SerialNumber: sn,
+		Subject: pkix.Name{
+			CommonName: name,
+			Organization:  []string{"DataCenter"},
+			Country:       []string{"US"},
+			Province:      []string{"CA"},
+			Locality:      []string{"SF"},
+			StreetAddress: []string{"MISSION ST."},
+			PostalCode:    []string{"94105"},
+		},
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().AddDate(10, 0, 0),
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
+		KeyUsage:     x509.KeyUsageDigitalSignature,
+		IsCA:                  false,
+		BasicConstraintsValid: true,
+	}
+	priv, _ := rsa.GenerateKey(rand.Reader, 2048)
+	pub := &priv.PublicKey
+
+	// Sign the certificate
+	cert_b, err := x509.CreateCertificate(rand.Reader, cert, ca, pub, catls.PrivateKey)
+
+	// Public key
+	certOut := new(bytes.Buffer)
+	pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: cert_b})
+	//fmt.Println(certOut.String())
+
+	// Private key
 	keyOut := new(bytes.Buffer)
 	pem.Encode(keyOut, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)})
 	//fmt.Println(keyOut.String())
--- cert/cert_test.go
+++ cert/cert_test.go
@@ -2,19 +2,90 @@ package cert_test
 
 import (
 	"testing"
-	_ "fmt"
+	"fmt"
 
-	wallet "github.com/Ankr-network/dccn-common/cert"
+	certmanager "github.com/Ankr-network/dccn-common/cert"
 )
 
 func TestGenerateKeys(t *testing.T) {
 	t.Log("Testing GenerateCA")
 
-	privateKey, publicKey, err := wallet.GenerateCA("test-ca.com")
+	caCert, caKey, err := certmanager.GenerateCA("myhub-ca.com")
 
 	if err == nil {
-		t.Logf("\nPrivate Key: %s\nPublic Key: %s\n", privateKey, publicKey)
+		t.Logf("\nCA Cert: %s\n CA Public Key: %s\n", caCert, caKey)
 	} else {
 		t.Error(err)
 	}
+}
+
+func TestGenerateServerCert(t *testing.T) {
+	t.Log("Testing GenerateServerCert")
+
+	caCert, caKey, err := certmanager.GenerateCA("myhub-ca.com")
+	if err != nil {
+		t.Error(err)
+	}
+
+	cert, privateKey, err := certmanager.GenerateServerCert("myhub.com", caCert, caKey)
+
+	if err == nil {
+		t.Logf("\ncert: %s\n private key: %s\n", cert, privateKey)
+	} else {
+		t.Error(err)
+	}
+}
+
+func TestGenerateClientCert(t *testing.T) {
+	t.Log("Testing GenerateClientCert")
+
+	caCert, caKey, err := certmanager.GenerateCA("myhub-ca.com")
+	if err != nil {
+		t.Error(err)
+	}
+
+	cert, privateKey, err := certmanager.GenerateClientCert("mydatacenter.com", caCert, caKey)
+
+	if err == nil {
+		t.Logf("\ncert: %s\n private key: %s\n", cert, privateKey)
+	} else {
+		t.Error(err)
+	}
+}
+
+func TestGenerateServerAndClientCert(t *testing.T) {
+	t.Log("Testing GenerateClientCert")
+
+	caCert, caKey, err := certmanager.GenerateCA("myhub-ca.com")
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println("CA CERT:")
+	fmt.Println(caCert)
+	fmt.Println("CA KEY:")
+	fmt.Println(caKey)
+
+	scert, sprivateKey, err := certmanager.GenerateServerCert("myhub.com", caCert, caKey)
+
+	if err == nil {
+		t.Logf("\ncert: %s\n private key: %s\n", scert, sprivateKey)
+	} else {
+		t.Error(err)
+	}
+	fmt.Println("SERVER CERT:")
+	fmt.Println(scert)
+	fmt.Println("SERVER KEY:")
+	fmt.Println(sprivateKey)
+
+	ccert, cprivateKey, err := certmanager.GenerateClientCert("mydatacenter.com", caCert, caKey)
+
+	if err == nil {
+		t.Logf("\ncert: %s\n private key: %s\n", ccert, cprivateKey)
+	} else {
+		t.Error(err)
+	}
+	fmt.Println("CLIENT CERT:")
+	fmt.Println(ccert)
+	fmt.Println("CLIENT KEY:")
+	fmt.Println(cprivateKey)
 }
