ea9335fb5b6a77cc6d46c3bb89786179c4cb4bf7
wweir <wweir@foxmail.com>
2019-04-22 13:25:45 +0800

    [SWDEV-122] refactor pgrpc alias logic

--- pgrpc/util.go
+++ pgrpc/util.go
@@ -4,10 +4,24 @@ import (
 	"net"
 
 	"github.com/hashicorp/yamux"
+	"google.golang.org/grpc"
 )
+
+type Session struct {
+	*yamux.Session
+
+	Name string
+	Opts []grpc.DialOption
+}
 
 type Hook interface {
 	OnAccept(key string, conn net.Conn) error
-	OnBuild(key string, conn *yamux.Session) error
-	OnClose(key string, conn *yamux.Session)
+	OnBuild(key string, conn *Session) error
+	OnClose(key string, conn *Session)
 }
+
+type emptyHook struct{}
+
+func (*emptyHook) OnAccept(key string, conn net.Conn) error { return nil }
+func (*emptyHook) OnBuild(key string, conn *Session) error  { return nil }
+func (*emptyHook) OnClose(key string, conn *Session)        {}
