90511345bd803cbee997ec9499ab10f8965c9d5a
Stanley Wu <35579150+fruit37@users.noreply.github.com>
2019-04-30 18:32:18 -0700

    [SWDEV-198] replace ankr.network with ankr.com

--- frontend/wallet-sdk/tests/index.js
+++ frontend/wallet-sdk/tests/index.js
@@ -1,104 +1,125 @@
-import { gen_key } from "../src/gen_key";
-import { get_balance, set_blockchain_addr as set_balance_bc_addr } from "../src/get_balance";
-import { send_coin, set_blockchain_addr as set_coin_bc_addr } from "../src/send_coin";
-import { get_send_history, get_recv_history, get_history, set_blockchain_addr as set_history_bc_addr } from "../src/get_history";
+import {gen_key} from '../src/gen_key';
+import {get_balance, set_blockchain_addr as set_balance_bc_addr} from '../src/get_balance';
+import {get_history, get_recv_history, get_send_history, set_blockchain_addr as set_history_bc_addr} from '../src/get_history';
+import {send_coin, set_blockchain_addr as set_coin_bc_addr} from '../src/send_coin';
 
 let element = null;
 
-const echo = (text) => {
-    element.appendChild(document.createElement("br"))
-    if (text != null) {
+const echo =
+    (text) => {
+      element.appendChild(document.createElement('br'))
+      if (text != null) {
         element.appendChild(document.createTextNode(text))
+      }
     }
-}
 
-document.getElementById("gen_keys_btn").onclick = async () => {
-    gen_key('me@example.com', 'brig alert rope welsh foss rang orb', (key) => {
-        element = document.getElementById("gen_keys");
-        echo();
-        echo("public key:");
-        echo(key.public_key);
-        echo();
-        echo("secret key:");
-        echo(key.private_key);
-        echo();
-        echo("wallet address:");
-        echo(key.addr);
-        echo();
-    });
-}
+              document.getElementById('gen_keys_btn')
+                  .onclick =
+        async () => {
+      gen_key(
+          'me@example.com', 'brig alert rope welsh foss rang orb', (key) => {
+            element = document.getElementById('gen_keys');
+            echo();
+            echo('public key:');
+            echo(key.public_key);
+            echo();
+            echo('secret key:');
+            echo(key.private_key);
+            echo();
+            echo('wallet address:');
+            echo(key.addr);
+            echo();
+          });
+    }
 
-document.getElementById("get_balance_btn").onclick = async () => {
-    set_balance_bc_addr("chain-stage.dccn.ankr.network:26657")
+                    document.getElementById('get_balance_btn')
+                        .onclick =
+            async () => {
+      set_balance_bc_addr('chain-stage.dccn.ankr.com:26657')
 
-    const balance = await get_balance('B508ED0D54597D516A680E7951F18CAD24C7EC9F');
-    element = document.getElementById("get_balance");
+      const balance =
+          await get_balance('B508ED0D54597D516A680E7951F18CAD24C7EC9F');
+      element = document.getElementById('get_balance');
 
-    echo();
-    echo("balance:");
-    echo(balance);
-}
+      echo();
+      echo('balance:');
+      echo(balance);
+    }
 
-document.getElementById("send_coin_btn").onclick = async () => {
-    set_coin_bc_addr("chain-stage.dccn.ankr.network:26657")
+                        document.getElementById('send_coin_btn')
+                            .onclick =
+                async () => {
+      set_coin_bc_addr('chain-stage.dccn.ankr.com:26657')
 
-    await send_coin('B508ED0D54597D516A680E7951F18CAD24C7EC9F', '0D9FE6A785C830D2BE66FE40E0E7FE3D9838456C', '88', 'wmyZZoMedWlsPUDVCOy+TiVcrIBPcn3WJN8k5cPQgIvC8cbcR10FtdAdzIlqXQJL9hBw1i0RsVjF6Oep/06Ezg==', 'wvHG3EddBbXQHcyJal0CS/YQcNYtEbFYxejnqf9OhM4=');
+          await send_coin(
+              'B508ED0D54597D516A680E7951F18CAD24C7EC9F',
+              '0D9FE6A785C830D2BE66FE40E0E7FE3D9838456C', '88',
+              'wmyZZoMedWlsPUDVCOy+TiVcrIBPcn3WJN8k5cPQgIvC8cbcR10FtdAdzIlqXQJL9hBw1i0RsVjF6Oep/06Ezg==',
+              'wvHG3EddBbXQHcyJal0CS/YQcNYtEbFYxejnqf9OhM4=');
 
-    set_balance_bc_addr("chain-stage.dccn.ankr.network:26657")
-    const balance = await get_balance('B508ED0D54597D516A680E7951F18CAD24C7EC9F');
-    element = document.getElementById("send_coin");
+      set_balance_bc_addr('chain-stage.dccn.ankr.com:26657')
+      const balance =
+          await get_balance('B508ED0D54597D516A680E7951F18CAD24C7EC9F');
+      element = document.getElementById('send_coin');
 
-    echo();
-    echo("new balance:");
-    echo(balance);
-}
+      echo();
+      echo('new balance:');
+      echo(balance);
+    }
 
-document.getElementById("get_send_history_btn").onclick = async () => {
-    set_history_bc_addr("chain-stage.dccn.ankr.network:26657")
+                            document.getElementById('get_send_history_btn')
+                                .onclick =
+                    async () => {
+      set_history_bc_addr('chain-stage.dccn.ankr.com:26657')
 
-    const historys = await get_send_history('B508ED0D54597D516A680E7951F18CAD24C7EC9F');
-    element = document.getElementById("get_send_history");
+      const historys =
+          await get_send_history('B508ED0D54597D516A680E7951F18CAD24C7EC9F');
+      element = document.getElementById('get_send_history');
 
-    echo();
-    echo("history:");
+      echo();
+      echo('history:');
     historys.forEach((history) => {
-        echo("From: " + history.from_addr)
-        echo("To: " + history.to_addr)
-        echo("Amount: " + history.amount)
-        echo("At: " + history.timestamp)
+        echo('From: ' + history.from_addr)
+    echo('To: ' + history.to_addr)
+    echo('Amount: ' + history.amount)
+    echo('At: ' + history.timestamp)
         echo()
     });
-}
-document.getElementById("get_recv_history_btn").onclick = async () => {
-    set_history_bc_addr("chain-stage.dccn.ankr.network:26657")
+    } document.getElementById('get_recv_history_btn')
+                                    .onclick =
+                        async () => {
+      set_history_bc_addr('chain-stage.dccn.ankr.com:26657')
 
-    const historys = await get_recv_history('0D9FE6A785C830D2BE66FE40E0E7FE3D9838456C');
-    element = document.getElementById("get_recv_history");
+      const historys =
+          await get_recv_history('0D9FE6A785C830D2BE66FE40E0E7FE3D9838456C');
+      element = document.getElementById('get_recv_history');
 
-    echo();
-    echo("history:");
+      echo();
+      echo('history:');
     historys.forEach((history) => {
-        echo("From: " + history.from_addr)
-        echo("To: " + history.to_addr)
-        echo("Amount: " + history.amount)
-        echo("At: " + history.timestamp)
+        echo('From: ' + history.from_addr)
+    echo('To: ' + history.to_addr)
+    echo('Amount: ' + history.amount)
+    echo('At: ' + history.timestamp)
         echo()
     });
-}
+    }
 
-document.getElementById("get_history_btn").onclick = async () => {
-    set_history_bc_addr("chain-stage.dccn.ankr.network:26657")
+                                    document.getElementById('get_history_btn')
+                                        .onclick = async () => {
+      set_history_bc_addr('chain-stage.dccn.ankr.com:26657')
 
-    const historys = await get_history('B508ED0D54597D516A680E7951F18CAD24C7EC9F');
-    element = document.getElementById("get_history");
+      const historys =
+          await get_history('B508ED0D54597D516A680E7951F18CAD24C7EC9F');
+      element = document.getElementById('get_history');
 
-    echo();
-    echo("history:");
+      echo();
+      echo('history:');
     historys.forEach((history) => {
-        echo("From: " + history.from_addr)
-        echo("To: " + history.to_addr)
-        echo("Amount: " + history.amount)
-        echo("At: " + history.timestamp)
+        echo('From: ' + history.from_addr)
+    echo('To: ' + history.to_addr)
+    echo('Amount: ' + history.amount)
+    echo('At: ' + history.timestamp)
         echo()
     });
-}
+    }
--- protos/default.go
+++ protos/default.go
@@ -2,13 +2,13 @@ package ankr_default
 
 // RabbitQ name
 const (
-	MQDeployApp          = "topic.deploy.app"
-	MQFeedbackApp        = "topic.feedback.app"
+	MQDeployApp           = "topic.deploy.app"
+	MQFeedbackApp         = "topic.feedback.app"
 	MQMail                = "topic.mail.handler"
-	AccessTokenValidTime  = 3600 * 2    // second   2 hours
+	AccessTokenValidTime  = 3600 * 2  // second   2 hours
 	RefreshTokenValidTime = 86400 * 7 // second   7 days
-	CleanupInterval       = 20      // minute
-	NoReplyEmailAddress   = "no-reply@ankr.network"
+	CleanupInterval       = 20        // minute
+	NoReplyEmailAddress   = "no-reply@ankr.com"
 )
 
 // To do: Remove this line when usr mgr is ready
@@ -20,7 +20,7 @@ const HeartBeatInterval = 60 // Default interval for heartbeat
 
 // Registry server name
 const (
-	AppMgrRegistryServerName = "go.micro.srv.v1.app"
+	AppMgrRegistryServerName  = "go.micro.srv.v1.app"
 	UserMgrRegistryServerName = "go.micro.srv.v1.user"
 	EmailRegistryServerName   = "go.micro.srv.v1.email"
 	DcMgrRegistryServerName   = "go.micro.srv.v1.data.center"
