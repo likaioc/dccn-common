481305237fb05d3486b4bdc553b622da494cbe91
Tony Wu <xiaowu@berkeley.edu>
2019-05-01 14:35:39 -0700

    [SWDEV-220]Update fliter api

--- protos/gateway/usermgr/v1/usermgr.pb.gw.go
+++ protos/gateway/usermgr/v1/usermgr.pb.gw.go
@@ -250,6 +250,23 @@ func request_UserMgr_GetERCDeposit_0(ctx context.Context, marshaler runtime.Mars
 
 }
 
+func request_UserMgr_SearchDeposit_0(ctx context.Context, marshaler runtime.Marshaler, client UserMgrClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq SearchDepositRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.SearchDeposit(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterUserMgrHandlerFromEndpoint is same as RegisterUserMgrHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterUserMgrHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -548,6 +565,26 @@ func RegisterUserMgrHandlerClient(ctx context.Context, mux *runtime.ServeMux, cl
 
 	})
 
+	mux.Handle("POST", pattern_UserMgr_SearchDeposit_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_UserMgr_SearchDeposit_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_UserMgr_SearchDeposit_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -577,6 +614,8 @@ var (
 	pattern_UserMgr_CreateercAddress_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"create_address"}, ""))
 
 	pattern_UserMgr_GetERCDeposit_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"deposit", "erc"}, ""))
+
+	pattern_UserMgr_SearchDeposit_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"deposit", "search"}, ""))
 )
 
 var (
@@ -605,4 +644,6 @@ var (
 	forward_UserMgr_CreateercAddress_0 = runtime.ForwardResponseMessage
 
 	forward_UserMgr_GetERCDeposit_0 = runtime.ForwardResponseMessage
+
+	forward_UserMgr_SearchDeposit_0 = runtime.ForwardResponseMessage
 )
