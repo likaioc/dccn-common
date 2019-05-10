b8bdfbf9cb7d48bf1c14e8be9f4ff0246cdc0fbe
song-liu <van20052005@hotmail.com>
2019-05-10 08:46:08 +0000

    SWDEV-212 add API GenerateEcdsaKeys
    API of ECDSA client and server cert
    add ecdsa sign/veriry, test_data, tests, README

--- cert/sign/sign.go
+++ cert/sign/sign.go
@@ -4,7 +4,9 @@ import (
 	"crypto"
 	"crypto/x509"
 	"crypto/rsa"
+	"crypto/ecdsa"
 	"fmt"
+	"math/big"
 	"errors"
 	"crypto/sha256"
 	"crypto/rand"
@@ -37,6 +39,28 @@ func RsaSign(privpem, input string) (string, error) {
 }
 
 /*
+use pem format string ECDSA private_key to sign the input
+*/
+func EcdsaSign(privpem, input string) (rSig, sSig string, err error) {
+	sum := sha256.Sum256([]byte(input))
+	priv, err := parseEcdsaPrivateKeyFromPemStr(privpem)
+	if (err != nil) {
+		fmt.Println(err)
+		return "", "", err
+	}
+
+	r, s, err := ecdsa.Sign(rand.Reader, priv, sum[:32])
+
+	if err != nil {
+		fmt.Println(err)
+		return  "", "", err
+	}
+
+	return r.String(), s.String(), nil
+
+}
+
+/*
 use pem format string RSA public_key to verify the input's signature.
 */
 func RsaVerify(pubpem, input, signature string) bool{
@@ -59,6 +83,38 @@ func RsaVerify(pubpem, input, signature string) bool{
 	return true
 }
 
+/*
+use pem format string ECDSA public_key to verify the input's signature.
+*/
+func EcdsaVerify(pubpem, input, signature1, signature2 string) bool{
+
+	rSig := new(big.Int)
+	rSig, ok := rSig.SetString(signature1, 10)
+	if !ok {
+		fmt.Println("SetString: error")
+		return false
+	}
+
+	sSig := new(big.Int)
+	sSig, ok = sSig.SetString(signature2, 10)
+	if !ok {
+		fmt.Println("SetString: error")
+		return false
+	}
+
+	ecPublicKey, err := parseEcdsaPublicKeyFromPemStr(pubpem)
+	if (err != nil) {
+		fmt.Println(err)
+		return false
+	}
+
+	sum := sha256.Sum256([]byte(input))
+	valid := ecdsa.Verify(ecPublicKey, sum[:32], rSig, sSig)
+
+	return valid
+}
+
+
 //=================================================================================
 /* helper functions */
 
@@ -69,6 +125,20 @@ func parseRsaPrivateKeyFromPemStr(privPEM string) (*rsa.PrivateKey, error) {
     }
 
     priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+    if err != nil {
+            return nil, err
+    }
+
+    return priv, nil
+}
+
+func parseEcdsaPrivateKeyFromPemStr(privPEM string) (*ecdsa.PrivateKey, error) {
+    block, _ := pem.Decode([]byte(privPEM))
+    if block == nil {
+            return nil, errors.New("failed to parse PEM block containing the privkey")
+    }
+
+    priv, err := x509.ParseECPrivateKey(block.Bytes)
     if err != nil {
             return nil, err
     }
@@ -89,6 +159,23 @@ func parseRsaPublicKeyFromPemStr(pubPEM string) (*rsa.PublicKey, error) {
         }
 
 	pub := cert.PublicKey.(*rsa.PublicKey)
+
+	return pub, nil
+}
+
+func parseEcdsaPublicKeyFromPemStr(pubPEM string) (*ecdsa.PublicKey, error) {
+	block, _ := pem.Decode([]byte(pubPEM))
+	if block == nil {
+		fmt.Println("failed to parse certificate PEM")
+		return nil, errors.New("failed to parse PEM block containing the cert")
+	}
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		fmt.Println("failed to parse certificate: ", err.Error())
+		return nil, err
+	}
+
+	pub := cert.PublicKey.(*ecdsa.PublicKey)
 
 	return pub, nil
 }
